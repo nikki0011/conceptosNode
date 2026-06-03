94d6652fe47273368dec2a1170bce31dc399c1de
Nair Paez <nairpaez@outlook.com>
2026-06-03 11:05:47 -0300

    agragamos los middeleare

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // const express = require('express')
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
-console.log('Este backend ya escuch 🌮')
+// agregar los middlewares necasarios
+app.use(cors()) // permitir conexiones remotas
+app.use(express.json()) // permite interpretar los datos que lleguen los datos que lleguen en la solicitud format json
+// agregar el otro middleware morgan
+
+
+
+console.log('Este backend ya esta escuchando 🌮')
+console.log('Nuevo mensaje')
 
 app.listen(PORT, ()=>{console.info(`Servidor activo en el puerto ${PORT}`)})

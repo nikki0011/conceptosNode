2c39294627d649c8ddcf0876cce85f75275bcf90
Nair Paez <nairpaez@outlook.com>
2026-06-08 11:10:21 -0300

    agregamos u archivo statico de bienvenida

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const express = require('express')
 import express from "express";
 import cors from "cors";
-import morgan from 'morgan'
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +12,12 @@ const PORT = 3000;
 app.use(cors()); // permitir conexiones remotas
 app.use(express.json()); // permite interpretar los datos que lleguen los datos que lleguen en la solicitud format json
 // agregar el otro middleware morgan
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname + '/public')
+// cargar el siguiente archivo en forma estatica
+app.use(express.static(__dirname + '/public'))
+
 
 console.log("Este backend ya esta escuchando 🌮");
 console.log("Nuevo mensaje");
@@ -18,12 +25,12 @@ console.log("Nuevo mensaje");
 // endpoints
 // http://localhost:3000/api/saludo
 app.get("/api/saludo", (req, res) => {
-//   console.log("Me llego una solicitud nueva");
-  const comida = ["manzana, pera, uvas,fruta rica de mexico , mango"]
+  //   console.log("Me llego una solicitud nueva");
+  const comida = ["manzana, pera, uvas,fruta rica de mexico , mango"];
   res.json({
     mensaje: "Conexion creada",
     saludo: "Bienvenido a nuestro backend",
-    comida: comida
+    comida: comida,
   });
 });
 

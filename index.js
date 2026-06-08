c681b5d0f557b0e855d749cd6e57cd8084bf2b29
Nair Paez <nairpaez@outlook.com>
2026-06-08 09:32:16 -0300

    cambios en endpoints

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 // const express = require('express')
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import morgan from 'morgan'
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 // agregar los middlewares necasarios
-app.use(cors()) // permitir conexiones remotas
-app.use(express.json()) // permite interpretar los datos que lleguen los datos que lleguen en la solicitud format json
+app.use(cors()); // permitir conexiones remotas
+app.use(express.json()); // permite interpretar los datos que lleguen los datos que lleguen en la solicitud format json
 // agregar el otro middleware morgan
+app.use(morgan('dev'))
 
+console.log("Este backend ya esta escuchando 🌮");
+console.log("Nuevo mensaje");
 
+// endpoints
+// http://localhost:3000/api/saludo
+app.get("/api/saludo", (req, res) => {
+//   console.log("Me llego una solicitud nueva");
+  const comida = ["manzana, pera, uvas,fruta rica de mexico , mango"]
+  res.json({
+    mensaje: "Conexion creada",
+    saludo: "Bienvenido a nuestro backend",
+    comida: comida
+  });
+});
 
-console.log('Este backend ya esta escuchando 🌮')
-console.log('Nuevo mensaje')
-
-app.listen(PORT, ()=>{console.info(`Servidor activo en el puerto ${PORT}`)})
+app.listen(PORT, () => {
+  console.info(`Servidor activo en el puerto ${PORT}`);
+});
